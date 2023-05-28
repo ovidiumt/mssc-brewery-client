5a6f7cd9e23640d5f0a33ffebc57d9c5ff9c1e96
Tudorache Mihai-Ovidiu <ovi99or@gmail.com>
2023-05-28 17:07:53 +0300

    added customer requests

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -47,5 +48,35 @@ class BreweryClientTest {
     @Test
     void deleteBeer(){
         client.deleteBeer(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerDto customerDto = client.getCustomerById(UUID.randomUUID());
+
+        assertNotNull(customerDto);
+    }
+
+    @Test
+    void saveNewCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().customerName("New Customer").build();
+
+        URI uri = client.saveNewCustomer(customerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void updateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().customerName("New Customer").build();
+
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void deleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
     }
 }
